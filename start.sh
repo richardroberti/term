53a45150a7e40fc495a0e36254484536ac2ba70a
Richard Robertov <richard@richard.local>
2023-05-17 03:33:56 +0300

    term-config

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-mkdir ~/.vim
 mkdir ~/.vim/colors
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
-cp colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
+cp colors ~/.vim
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 zsh
 
-source .zshrc
+source ~/.zshrc
+
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
