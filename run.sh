f3d40110d0a812c3fb5768d688e2bf3f774530f2
Richard Robertov <richard@richard.local>
2023-05-17 21:01:07 +0300

    term-config

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cp .* ~/
+cp -r .* ~/
 chsh -s /bin/zsh && source ~/.zshrc && source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
